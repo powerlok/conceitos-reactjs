05a8abd56fad610d1bfe1ab8e20a976cfb4b4695
PAULO EDUARDO DE MELO LAREDO <paulo.laredo@redeoba.com.br>
2020-04-08 23:57:09 -0300

    Add end Remove Repository

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
 
+import api from './services/api';
+
 function App() {
+  const [repository, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      console.log(response);
+      setRepository(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      url: "https://github.com/Rocketseat/umbriel",
+      title: `Umbriel ${Date.now()}`,
+      techs: ["Node", "Express", "TypeScript"]
+    });
+
+    const project = response.data;
+
+    setRepository([...repository, project]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try{
+      await api.delete(`repositories/${id}`)
+
+      setRepository(repository.filter(repo => repo.id != id));
+    }catch(e) {
+      alert('Erro ao deletar caso, tente novamente');
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repository.map(rep => (
+          <li key={rep.id}>
+            {rep.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(rep.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
